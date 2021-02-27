b19ff1ff1f18b830fd2a356e97d68d503437848b
Pavel Fokin <fokinpavel2007@outlook.com>
2021-02-27 19:07:54 +0300

    New webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,45 @@
 ﻿
- 
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const path = require ( 'path' );
 module.exports = {
-    entry: './src/index.js',
-    output: {
-    path:path.resolve(__dirname,'dist'),
-    filename: '[name].js'
-    }
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
 };
